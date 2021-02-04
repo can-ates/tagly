d806ff9c5be8b856b21510bdda2f6c0523a36df2
Mutlu Can ATES <canates94@gmail.com>
2021-02-04 15:48:50 +0300

    react version changed

--- src/TagsComponent/Tags.tsx
+++ src/TagsComponent/Tags.tsx
@@ -1,17 +1,26 @@
 import React from "react";
 
 import "./mystyles.scss";
+import ContentEditable from 'react-contenteditable'
 
 interface Props {
-  text: string
+  lol: string
 }
 
-const TagCan: React.FC<Props> = ({ text }) => (
+const TagCan: React.FC<Props> = ({ lol }) => {
+  const text = React.useRef(null)
+
+  const handleChange = (evt) => {
+    text.current = evt.target.value;
+  }
+
+  return (
   <div
     
   >
-    <p>lol</p>
+    <ContentEditable onChange={handleChange} html={text.current} />
+    <p>{lol}</p>
   </div>
-);
+)};
 
 export default TagCan;
